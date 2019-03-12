2d5234ab794445e067bbc085014d7b90e8847dd2
Kevin Boese <kevinboese5@gmail.com>
2019-03-12 15:06:31 -0700

    Added basic accessors and constructor

--- binary-tree/c++/binaryTree.cpp
+++ binary-tree/c++/binaryTree.cpp
@@ -3,6 +3,34 @@
 
 #include "binaryTree.h"
 
-class BinaryTree {
+BinaryTree::BinaryTree(int data) {
+    struct Node *tmp = new Node;
+    tmp->parent = nullptr;
+    tmp->left = nullptr;
+    tmp->right = nullptr;
+    tmp->data = data;
+
+    // Assign root node
+    this->root = tmp;
+    // Set height and size
+    this->height = 0;
+    this->size = 1;
+}
+
+/* ***** Binary Tree functions ***** */
+void BinaryTree::insert(int data, struct Node *n) {
     
-};
+}
+
+/* ***** Accessors ***** */
+int BinaryTree::getHeight() {
+    return this->height;
+}
+
+long int BinaryTree::getSize() {
+    return this->size;
+}
+
+struct Node *BinaryTree::getRoot() {
+    return this->root;
+}
--- binary-tree/c++/binaryTree.h
+++ binary-tree/c++/binaryTree.h
@@ -8,4 +8,27 @@ struct Node {
     int data;
 };
 
+class BinaryTree {
+
+public:
+    // Constructors
+    BinaryTree(int data);
+
+    // Tree functions
+    void insert(int data, struct Node *n);
+
+    // Accessors
+    int getHeight();
+    long int getSize();
+    struct Node *getRoot();
+
+private:
+    // Root node
+    struct Node *root;
+    // Height of the tree
+    int height;
+    // Total number of nodes in the tree
+    long int size;
+};
+
 #endif
