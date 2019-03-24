54015eb85bae2ddfd28ce538214964cac4a39f5b
Kevin Boese <kevinboese5@gmail.com>
2019-03-23 21:35:38 -0700

    Added testing for findLeftmostParent

--- binary-heap/c++/minHeapTest.cpp
+++ binary-heap/c++/minHeapTest.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 void testCreateHeap();
+void testSingleInsert();
+void testSequentialInsert();
 
 // Helper functions
 void printPassed();
 
 int main(int argc, char **argv) {
     testCreateHeap();
+    testSingleInsert();
+    testSequentialInsert();
     return 0;
 }
 
@@ -23,6 +27,44 @@ void testCreateHeap() {
     printPassed();
 }
 
+void testSingleInsert() {
+    printf("\nTest: %s\n", __func__);
+    int el = -1;
+    // Leftmost parent
+    struct Node *lmp;
+
+    MinHeap heap{};
+    heap.insert(el, heap.getRoot());
+    // The LMP is the root in this example
+    lmp = heap.findLeftmostParent(heap.getRoot());
+
+
+    assert(heap.getSize() == 1);
+    assert(heap.getRoot()->data == el);
+    assert(lmp->data == el);
+
+    heap.printDFS(heap.getRoot());
+
+    printPassed();
+}
+
+void testSequentialInsert() {
+    printf("\nTest: %s\n", __func__);
+
+    MinHeap heap{};
+    int lowerBound = 1;
+    int upperBound = 10;
+
+    // Insert elements
+    for (int i = lowerBound; i < upperBound; i++) {
+        heap.insert(i, heap.getRoot());
+    }
+
+    
+
+    printPassed();
+}
+
 void printPassed() {
-    cout << "***** PASS *****\n";
+    cout << "\n***** PASS *****\n";
 }
